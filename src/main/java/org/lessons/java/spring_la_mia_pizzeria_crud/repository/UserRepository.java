d26fb67fea464543b062b61300b8fd78cdad7cb0
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-09-17 12:59:48 +0200

    ADD UserRepository

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/UserRepository.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.repository;
 
-public class UserRepository {
-    
+import java.util.Optional;
+
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // Recupero le informazioni in base al username
+    Optional<User> findByUsername(String string);
 }
